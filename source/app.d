1b34eccb6102d1987113e514eafce7e7720a2980
felipe bastos <felipebastosweb@gmail.com>
2021-01-21 11:16:45 -0300

    Update app.d

--- source/app.d
+++ source/app.d
@@ -1,12 +1,22 @@
 import std.stdio;
 
 import std.parallelism;
-import neural.node;
+import neural.brain;
 
 
 
 void main()
 {
+	auto knowList = know_list();
+	
+	foreach (ref elem; parallel(knowList)) {
+		func = elem[1];
+		auto mytask = task!func();
+		mytask.executeInNewThread();
+		bool result = mytask.yieldForce;
+		writeln(result);
+	}
+	
 	/*
 	auto logs = new double[1_000];
 
